61f7cdc235ea6925e221269653e48f57b0a49ffb
Unchain7997 <156478836+Unchain7997@users.noreply.github.com>
2024-01-12 19:49:26 -0700

    Add files via upload

--- Homework1.cs
+++ Homework1.cs
@@ -1,9 +1,16 @@
-﻿namespace HelloWorld;
+﻿/*
+Author: Diana Benitez
+The first C# program for Hello World.
+*/
+namespace HelloWorld;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        double x = 2.5;
+        double y = 3.3;
+        double z = 4*x*x + 3*y;
+        Console.WriteLine(z);
     }
 }
