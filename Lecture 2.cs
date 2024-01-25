a5ad54109674b309b472c31426e13f9e5cecfcce
Unchain7997 <156478836+Unchain7997@users.noreply.github.com>
2024-01-24 20:54:39 -0700

    Upload Week2-Homework2

---
+++ Lecture 2.cs
@@ -0,0 +1,68 @@
+﻿namespace Lecture2;
+
+class Program
+{
+    static void Main(string[] args) {
+        
+        /*Question 1:
+        Console.WriteLine("Please input a letter grade:");
+        string input = Console.ReadLine();
+        
+        switch (input.ToUpper()) {
+            case "A": Console.WriteLine("GPA point: 4");
+            break;
+            case "B": Console.WriteLine("GPA point: 3");
+            break;
+            case "C": Console.WriteLine("GPA point: 2");
+            break;
+            case "D": Console.WriteLine("GPA point: 1");
+            break;
+            case "F": Console.WriteLine("GPA point: 0");
+            break;
+            default: Console.WriteLine("Wrong Letter Grade!");
+            break;
+        }*/
+
+        /*Question 2:
+        Console.WriteLine("Please input the first num:");
+        string num1 = Console.ReadLine();
+        int first_num = Convert.ToInt16(num1);
+        Console.WriteLine("Please input the second num:");
+        string num2 = Console.ReadLine();
+        int second_num = Convert.ToInt16(num2);
+        Console.WriteLine("Please input the third num:");
+        string num3 = Console.ReadLine();
+        int third_num = Convert.ToInt16(num3);
+
+        if(first_num<second_num){
+            if(first_num<third_num){
+            Console.WriteLine($"The smallest value is: {first_num}");
+            }
+            else{
+            Console.WriteLine($"The smallest value is: {third_num}");
+            }
+        }
+        else{
+            if(second_num<third_num){
+            Console.WriteLine($"The smallest vale is: {second_num}");
+            }
+            else{
+            Console.WriteLine($"The smallest value is: {third_num}");
+            }
+        }*/
+
+        /*Question 3:
+        Console.WriteLine("Please input a year:");
+        string input_year = Console.ReadLine();
+        int year = Convert.ToInt16(input_year);
+
+        if ((year %4==0 && year %100!=0) || (year %4==0 && year %100==0 && year %400==0)){
+            Console.WriteLine($"{year} is a Leap Year.");
+        } 
+        else {
+            Console.WriteLine($"{year} is not a Leap Year.");
+        }*/
+
+
+    }
+}
