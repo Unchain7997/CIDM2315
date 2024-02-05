083658cb650faa5245ce474a8a8502a9cc1421d7
Unchain7997 <156478836+Unchain7997@users.noreply.github.com>
2024-02-05 15:10:39 -0700

    Upload Homework3

---
+++ Homework3.cs
@@ -0,0 +1,56 @@
+﻿namespace Homework3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*Console.WriteLine("Homework 3. Question 1");
+        Console.WriteLine("Please input a number:");
+        int num = Convert.ToInt16(Console.ReadLine());
+        for (int i=2; i<num; i++){
+            if (num%i==0){
+            Console.WriteLine($"{num} is not Prime");
+            break;
+        }
+           else{
+            Console.WriteLine($"{num} is Prime");
+            break;
+        }
+        }*/
+
+
+        /*Console.WriteLine("Homework 3. Question 2");
+        Console.WriteLine("Assign an int value to N:");
+        int num = Convert.ToInt16(Console.ReadLine());
+        for (int row=0; row<5; row++){
+            for (int col=0; col<5; col++){
+                Console.Write('#');
+            }
+            Console.WriteLine("");
+        }*/
+
+        /*Console.WriteLine("Homework 3. Question 3");
+        Console.WriteLine("Assign an int value to N:");
+        int num = Convert.ToInt16(Console.ReadLine());
+        for (int row = 0; row<5; row++){
+            for (int col = 0; col<5; col++){
+                if (row>=col)
+                Console.Write('*');
+            }
+            Console.WriteLine("");
+        }*/
+
+        
+       /*Console.WriteLine("Homework 3. Bonus Question");
+        Console.WriteLine("Assign an int value to N:");
+        int num = Convert.ToInt16(Console.ReadLine());
+        for (int row = 1; row<=num; row++){
+            for (int col = 1; col<=num; col++){
+                if (row>=col)
+                Console.Write(row);
+            }
+            Console.WriteLine("");
+        }*/
+
+    }
+}
