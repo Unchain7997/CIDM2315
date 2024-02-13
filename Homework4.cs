6c5f9188b07a7782ec88a18e1f517c92a2e6ec7a
Unchain7997 <156478836+Unchain7997@users.noreply.github.com>
2024-02-12 22:39:31 -0700

    Uploading Homework4

---
+++ Homework4.cs
@@ -0,0 +1,44 @@
+﻿namespace Homework4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+           /*Console.WriteLine("Homework 4 - Q1");
+        int input1 = Convert.ToInt32(Console.ReadLine());
+        int input2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Largest number is: " + Largest(input1, input2));
+    }
+    static int Largest(int input1, int input2){
+        return input1 > input2 ? input1 : input2;*/
+
+    /*Console.WriteLine("Homework 4 - Q2");
+    Console.WriteLine("Please, enter a numerical value for N and indicate if the shape goes left or right");
+    int number = Convert.ToInt32(Console.ReadLine());
+    string shape = Console.ReadLine();
+    Console.WriteLine($"N is: {number}; shape is {shape}");
+    ShapedStars(number,shape);
+    }
+    static void ShapedStars(int number, string shape){
+        if(shape == "left"){
+            for (int i=1; i<=number; i++){
+                for (int s=0; s<i; s++){
+                    Console.Write('*');
+                }
+            Console.WriteLine("");
+            }
+        }
+            
+        else if (shape == "right"){
+            for (int i=1; i<=number; i++){
+            //Trying to print spaces n-i times but couln't make it
+            Console.Write("");
+    
+            for (int s=0; s<i; s++){
+            Console.Write("*");
+            }
+        Console.WriteLine();
+    }
+}*/
+    }
+}
