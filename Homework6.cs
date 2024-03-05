c9b83824f1d864b4dddd03cbcd7fab4c92001a48
Unchain7997 <156478836+Unchain7997@users.noreply.github.com>
2024-03-04 19:14:42 -0700

    Uploading Homework6 in one file

--- Homework6.cs
+++ Homework6.cs
@@ -35,4 +35,45 @@ class Program
             Console.WriteLine($"The total grade of {s1.studentName} and {s2.studentName} is: {totalGrade}");
         
         }
+
+
+    class Professor{
+        public string profName;
+        public string classTeach;
+        private double salary;
+        public void SetSalary(double salary_amount){
+            salary = salary_amount;
+        }
+        public double GetSalary() {
+            return salary;
+        }
+        public void PrintProfessor(){
+            Console.WriteLine($"Professor {profName} teaches {classTeach}, and the salary is: {GetSalary()}");
+        }
+        public static double CalculateSalaryDifference(Professor p1, Professor p2){
+            double salaryDifference = (p1.GetSalary() - p2.GetSalary());
+            return salaryDifference;
+        }
+
     }
+
+    class Student{
+        public string studentName;
+        public string classEnroll;
+        private double studentGrade;
+        public void SetGrade(double newGrade){
+            studentGrade = newGrade;
+        }
+        public double GetGrade() {
+            return studentGrade;
+        }
+        public void PrintStudent(){
+            Console.WriteLine($"Student {studentName} enrolls {classEnroll}, and the grade is: {GetGrade()}");
+        }
+        public static double CalculateTotalGrade(Student s1, Student s2){
+            double totalGrade = (s1.GetGrade() + s2.GetGrade());
+            return totalGrade;
+        }
+
+    }
+}
